44926f6c1bd6be844eea5c02bebe3385215416cd
Mayur Sonawane <sonawanemayur2907@gmail.com>
2026-03-30 15:16:50 +0530

    5th practical

---
+++ OneDirectionalArray.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	
+	int array[10]= { 5 ,10 , 15 , 20 , 25 , 30 , 35 , 40 , 45, 50};
+	int i;
+	
+	printf("Element of the array are: \n");
+	
+	
+	for(i = 0 ; i < 10 ; i++) {
+	
+	   printf("array[%d] = %d \n", i , array[i]);
+}
+
+return 0;
+}
